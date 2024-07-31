03f485bc07e5d4ade3bd5f4bf002be855b6a85ce
MohammadAmmad <117718535+Ammadgithub123@users.noreply.github.com>
2024-07-31 16:50:16 +0500

    Initial commit

---
+++ Controller2/Controller123/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controller2.Controller123
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            //viewResult, PartialViewsResult, JsonResult 
+        }
+        public string AZ()
+        {
+            return "M.AMMAD";
+        }
+        public int DisplayId(int id)
+        {
+            return id;
+        }
+    }
+}
